7326f53ba7f27ba929733026e1162efde978310e
Priyanka Babar <127826287+priyanka-babar@users.noreply.github.com>
2024-11-15 16:12:56 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <>
-  <h2> welcome react App</h2>
-  
+  return (
+    <>
+      <h2> welcome react App</h2>
 
-  </>;
+      <h3> priyanka babar</h3>
+    </>
+  );
 }
 
 export default App;
